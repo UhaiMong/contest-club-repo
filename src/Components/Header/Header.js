9e436ace7b2cc1ec5790e03e72f99f770a4ebf71
UhaiMong <uhaimarma2016@gmail.com>
2022-09-28 23:02:05 +0600

    header completed

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h3>Programming Contest Club</h3>
+            <h4>Select your level and Jump to code.</h4>
+        </div>
+    );
+};
+
+export default Header;
